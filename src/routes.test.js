5ba3b5e0de9fa949054fd56a413ce056e8744b78
rpererah <rpererah@gmail.com>
2024-06-06 13:42:40 -0300

    test children routes

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './paginas/Principal/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import Cartoes from './componentes/Cartoes';
 
-describe('Rotas', () => {
-  test('deve renderizar a rota principal', () => {
+describe('Routes', () => {
+  test('render default route', () => {
     render(<App />, { wrapper: BrowserRouter });
     const usuario = screen.getByText('Olá, Joana :)!');
     expect(usuario).toBeInTheDocument();
+  });
+
+  test('render cards route ', () => {
+    const route='/cartoes'
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="cartoes" element={<Cartoes />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const myCards=screen.getByText('Meus cartões')
+    expect(myCards).toHaveTextContent('Meus cartões')
+
   });
 });
