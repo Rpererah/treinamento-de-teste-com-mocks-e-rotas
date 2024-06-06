64e696e0fd1c4edc48eebcf8be7288415df1d1a2
rpererah <rpererah@gmail.com>
2024-06-06 15:22:37 -0300

    must navigate to clicked link

--- src/paginas/Principal/App.test.js
+++ src/paginas/Principal/App.test.js
@@ -1,4 +1,5 @@
 import {
+    findByText,
   getAllByRole,
   getByRole,
   render,
@@ -7,6 +8,7 @@ import {
 import userEvent from '@testing-library/user-event';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './../../routes'
 
 describe('App test integrity', () => {
   test('Must allow add a transation in Extrato Component', () => {
@@ -25,4 +27,26 @@ describe('App test integrity', () => {
 
     expect(newTransition).toContainElement(itemExtract)
   });
+
+
+  test('must navigate to clicked link cards',async ()=>{
+    render(<AppRoutes />, {wrapper:BrowserRouter})
+    const linkPageCards=screen.getByText('Cartões')
+    expect(linkPageCards).toBeInTheDocument()
+
+    userEvent.click(linkPageCards)
+    //using *find* instead of *get* because the call not is immediately we need a async function to resolve
+    const titleOfPageCards=await screen.findByText('Meus cartões')
+    expect(titleOfPageCards).toBeInTheDocument()
+  })
+
+
+  test('must navigate to clicked link Investimentos',async ()=>{
+    render(<AppRoutes />, {wrapper:BrowserRouter})
+    const linkPageInvestimentos=screen.getByText('Investimentos')
+    expect(linkPageInvestimentos).toBeInTheDocument()
+    userEvent.click(linkPageInvestimentos)
+    const titleOfPageInvestiments=await screen.findByRole('heading',{level:2,name:'Investimentos'})
+    expect(titleOfPageInvestiments).toBeInTheDocument()
+  })
 });
